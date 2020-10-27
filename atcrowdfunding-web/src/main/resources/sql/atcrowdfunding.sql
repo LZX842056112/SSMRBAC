e661378ff83eecdf8ad90238cd01cf536e7f4e78
李宗效 <842056112@qq.com>
2020-10-27 17:31:25 +0800

    添加sql文件

---
+++ atcrowdfunding-web/src/main/resources/sql/atcrowdfunding.sql
@@ -0,0 +1,102 @@
+/*
+SQLyog Ultimate v12.09 (64 bit)
+MySQL - 5.7.24-log : Database - atcrowdfunding
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`atcrowdfunding` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_bin */;
+
+USE `atcrowdfunding`;
+
+/*Table structure for table `t_permission` */
+
+DROP TABLE IF EXISTS `t_permission`;
+
+CREATE TABLE `t_permission` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  `pid` int(11) DEFAULT NULL,
+  `url` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  `icon` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+/*Data for the table `t_permission` */
+
+insert  into `t_permission`(`id`,`name`,`pid`,`url`,`icon`) values (1,'系统菜单',0,NULL,'glyphicon glyphicon-equalizer'),(2,'控制面板',1,NULL,'glyphicon glyphicon-dashboard'),(3,'权限管理',1,NULL,'glyphicon glyphicon glyphicon-tasks'),(4,'用户维护',3,'/user/index','glyphicon glyphicon-user'),(5,'角色维护2',3,'/role/index','glyphicon glyphicon-king'),(6,'许可维护',3,'/permission/index','glyphicon glyphicon-lock'),(7,'增加用户',4,NULL,NULL),(8,'修改用户',4,NULL,NULL);
+
+/*Table structure for table `t_role` */
+
+DROP TABLE IF EXISTS `t_role`;
+
+CREATE TABLE `t_role` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+/*Data for the table `t_role` */
+
+insert  into `t_role`(`id`,`name`) values (1,'PM-项目经理'),(2,'SE-软件工程师'),(3,'PG-程序员'),(4,'TL-组长'),(5,'GL-组长'),(6,'QC-品质控制'),(7,'SA-软件架构师'),(8,'CMO/CMS-配置管理'),(9,'SYSTEM-系统管理');
+
+/*Table structure for table `t_role_permission` */
+
+DROP TABLE IF EXISTS `t_role_permission`;
+
+CREATE TABLE `t_role_permission` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `roleid` int(11) DEFAULT NULL,
+  `permissionid` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+/*Data for the table `t_role_permission` */
+
+insert  into `t_role_permission`(`id`,`roleid`,`permissionid`) values (1,3,1),(2,3,3),(3,3,5),(4,3,6);
+
+/*Table structure for table `t_user` */
+
+DROP TABLE IF EXISTS `t_user`;
+
+CREATE TABLE `t_user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  `loginacct` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  `userpswd` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  `email` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  `createtime` char(19) COLLATE utf8_bin DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+/*Data for the table `t_user` */
+
+insert  into `t_user`(`id`,`username`,`loginacct`,`userpswd`,`email`,`createtime`) values (1,'zs','zs','zs','zs.com','2020-10-10 17:31:04'),(2,'ls','ls','123456','ls.com','2020-10-10 17:31:22'),(3,'ww','ww','123456','ww.com','2020-10-10 17:31:39'),(4,'zl','zl','123456','zl.coma','2020-10-10 17:32:44');
+
+/*Table structure for table `t_user_role` */
+
+DROP TABLE IF EXISTS `t_user_role`;
+
+CREATE TABLE `t_user_role` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `userid` int(11) DEFAULT NULL,
+  `roleid` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+/*Data for the table `t_user_role` */
+
+insert  into `t_user_role`(`id`,`userid`,`roleid`) values (1,1,3);
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
